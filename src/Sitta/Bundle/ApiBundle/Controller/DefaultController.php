715b1a0ee58ffc5ac6a1993398204d064440d37a
Patrick Leng <lgy22106@gmail.com>
2013-09-28 17:42:30 -0700

    hi

--- src/Sitta/Bundle/ApiBundle/Controller/DefaultController.php
+++ src/Sitta/Bundle/ApiBundle/Controller/DefaultController.php
@@ -4,10 +4,29 @@ namespace Sitta\Bundle\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use TesseractOCR;
+
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('SittaApiBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function getAction() {
+
+        $filepath = __DIR__.'/../../../../web/hello.png';
+        $ocr = new TesseractOCR();
+
+        $text = $ocr->recognize($filepath);
+
+        
+
+
+
+        $text = 'hi';
+        $response = new Response(json_encode(array('value' => $text)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
